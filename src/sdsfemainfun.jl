eadc105c8c0ec30e52cf09aec6ddf0d900fae089
advancehs <1019753743@qq.com>
2026-02-15 16:37:12 +0800

    init

--- src/sdsfemainfun.jl
+++ src/sdsfemainfun.jl
@@ -1746,7 +1746,8 @@ function sfmodel_fit(sfdat::DataFrame) #, D1::Dict = _dicM, D2::Dict = _dicINI, 
                   column_labels=["", "Var.", "Coef.", "Std.Err.", "z", "P>|z|", 
                           "95%CI_l", "95%CI_u"],
                   formatters = [(v, i, j) -> (j in 3:8 && v isa Number) ? @sprintf("%5.4f", v) : v],
-                  compact_printing = true)
+                  compact_printing = false,crop = :none,columns_width = 0
+                  )
       println()
 
 
@@ -1780,7 +1781,8 @@ function sfmodel_fit(sfdat::DataFrame) #, D1::Dict = _dicM, D2::Dict = _dicINI, 
            pretty_table(auxtable[1:rn,:],
                         column_labels=["", "Coef.", "Std.Err."],
                         formatters = [(v, i, j) -> (j in 2:3 && v isa Number) ? @sprintf("%5.4f", v) : v],
-                        compact_printing = true)
+                        compact_printing = false,crop = :none,columns_width = 0
+                        )
 
 
            print("\nTable format: "); printstyled("$(sf_table)"; color=:yellow); println(". Use sfmodel_opt() to choose between text, html, and latex.")
